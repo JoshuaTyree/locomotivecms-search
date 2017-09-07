45137f5c39f5d540a37f6bb7b3a969bb5735d540
Joshua Tyree <joshuat@createthebridge.com>
2017-09-06 20:18:53 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   namespace :locomotive do
 
-    get 'search' => 'search#index', as: :search
+    scope ':site_handle' do
+      get 'search' => 'search#index', as: :search
+    end
+
 
   end
 
